cdd49eda06d895e59e48e1b16b7632e93148659c
Carlos Rudyelissom <cdnrudy@gmail.com>
2026-02-05 17:35:28 -0300

    projeto missao impossivel

---
+++ Gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ style: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function html() {
+    return gulp.src('*.html')
+        .pipe(gulp.dest('./dist'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts, html);
+exports.watch = function () {
+    gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', { ignoreInitial: false }, gulp.parallel(scripts))
+    gulp.watch('./src/images/**/*', images);
+    gulp.watch('./*.html', html);
+}
+
---
+++ src/scripts/main.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    });
+
+    //seção atrações, programação das abas
+
+    //seção FAQ accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+});
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
